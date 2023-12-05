e8f04c2663cb2df8242a94ebc8fa4d2312e5812d
MarsRon <71264052+MarsRon@users.noreply.github.com>
2023-12-05 18:16:13 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,29 @@
 import markovify
+from time import time
 
-with open('./aqua.json', 'r') as file:
+MODEL_FILE = "./aqua.json"
+
+print(f"Loading model file {MODEL_FILE}")
+start = time()
+with open(MODEL_FILE, 'r') as file:
   aqua = markovify.Text.from_json(file.read())
+end = time()
+print(f"Aqua model loaded in {(end-start)*1000:.3f}ms\n")
 
 # Generate sentences
+print("Generate sentences")
 for i in range(5):
-  print(aqua.make_sentence())
+  start = time()
+  sentence = aqua.make_sentence()
+  end = time()
+  print(f"{i+1}. {(end-start)*1000:.3f}ms: {sentence}")
 
 print()
 
 # Generate short sentences less than 50 characters
+print("Generate short sentences")
 for i in range(5):
-  print(aqua.make_short_sentence(max_chars=50))
+  start = time()
+  sentence = aqua.make_short_sentence(max_chars=50)
+  end = time()
+  print(f"{i+1}. {(end-start)*1000:.3f}ms: {sentence}")
